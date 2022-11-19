7a2fb29a6d8e321708ba8542995f4c7c5d05f1e1
Dejan Vasic <dejanvasic24@gmail.com>
2022-11-19 19:18:04 +1100

    Improving the layout and some placeholders

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,33 @@
 import Container from './Container';
 
+/*
+sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100 text-base-content shadow-sm
+sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 text-primary-content
+*/
+
 export default function Navbar() {
   return (
-    <nav className="navbar w-full">
-      <Container>
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">SES - Storm Electrical Solutions</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal p-0">
-            <li>
-              <a>Services</a>
-            </li>
-            <li>
-              <a>About Us</a>
-            </li>
-            <li>
-              <a>Contact Us</a>
-            </li>
-          </ul>
-        </div>
-      </Container>
-    </nav>
+    <div className="sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 text-primary-content">
+      <nav className="navbar w-full">
+        <Container>
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">Logo</a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal p-0">
+              <li>
+                <a>Services</a>
+              </li>
+              <li>
+                <a>About Us</a>
+              </li>
+              <li>
+                <a>Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </nav>
+    </div>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,39 @@ class MyDocument extends Document {
   render() {
     return (
       <Html data-theme="light">
-        <Head />
+        <Head>
+          <title>SES - Storm Electrical Solutions - Melbourne Electricians</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Melbourne Electricians. Free Quotes. Lighting. Testing. Data. Air Conditioning. Emergency Call out. Upgrade your old Halogen lights for Free!*"
+          />
+          <meta name="robots" content="noodp" />
+          <link rel="canonical" href="http://sesmelbourne.com.au/" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Storm Electrical Solutions Melbourne" />
+          <meta
+            property="og:description"
+            content="Storm Electrical Solutions provides many services to all areas of Melbourne, with over 12 years experience in the electrical industry. SES can provide you with an extensive range of expertise within the commercial, Industrial and Residential sector. Whether it is installations, maintenance, fault finding, testing, large or small SES can cater for your needs with its friendly, professional, neat, reliable and efficient service at a phone call away at very competitive rates."
+          />
+          <meta property="og:url" content="http://sesmelbourne.com.au/" />
+          <meta property="og:site_name" content="Storm Electrical Melbourne" />
+          <meta
+            property="og:image"
+            content="http://sesmelbourne.com.au/wp-content/uploads/2016/03/SES-Storm-Electrical-Solutions.gif"
+          />
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:description"
+            content="Melbourne Electricians. Free Quotes. Lighting. Testing. Data. Air Conditioning. Emergency Call out. Upgrade your old Halogen lights for Free!"
+          />
+          <meta name="twitter:title" content="SES - Storm Electrical Solutions - Melbourne Electricians" />
+          <meta
+            name="twitter:image"
+            content="http://sesmelbourne.com.au/wp-content/uploads/2016/03/SES-Storm-Electrical-Solutions.gif"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
