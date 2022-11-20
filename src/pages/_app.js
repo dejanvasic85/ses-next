ac6993633221f651962f349cfc75d3ed4c4077ed
Dejan Vasic <dejanvasic24@gmail.com>
2022-11-20 15:09:49 +1100

    Fixing the build

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,10 @@ import { Footer, Navbar } from '../components';
 import '../../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  console.log('pageProps', pageProps);
   return (
     <>
-      <Navbar contactPhone={pageProps.contact.phone} />
+      <Navbar contactPhone={pageProps.contact?.phone} />
       <Component {...pageProps} />
       <Footer />
     </>
