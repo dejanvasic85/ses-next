40659aa9249449f4fae19daf51a9913f3289d3e3
Dejan Vasic <dejanvasic24@gmail.com>
2022-11-26 14:45:11 +1100

    Adding an about content

--- src/lib/content.js
+++ src/lib/content.js
@@ -1,6 +1,11 @@
 import { faker } from '@faker-js/faker';
 
 export const content = {
+  about: [
+    'At Storm Electrical Solutions we pride ourselves on being a forward thinking and innovative company offering a customer focused, quality service within the electrical industry.',
+    'We can offer a great deal of experience and expertise in a wide range of commercial, industrial and residential projects large or small including installations, maintenance, fault finding and testing.',
+    'We keep abreast of the latest developments in renewable energy and the electrical industry to ensure our customers get the best possible service available.',
+  ],
   contact: {
     phone: '+61 415 338 040',
   },
