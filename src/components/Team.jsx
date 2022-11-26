fe8d736c003c5d0cbecf7aabed54c26cdfbc3bfe
Dejan Vasic <dejanvasic24@gmail.com>
2022-11-26 14:48:06 +1100

    Using flex box for the team members

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -10,9 +10,9 @@ export function Team({ introduction, members }) {
           <Heading level={2}>Meet our Team</Heading>
           <p className="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">{introduction}</p>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 lg:gap-8">
+        <div className="flex flex-col flex-wrap justify-center lg:flex-row gap-4 lg:gap-8">
           {members.map(({ avatar, fullName, role }) => (
-            <div className="flex flex-col items-center bg-slate-100 rounded-lg p-4 lg:p-8">
+            <div className="flex flex-col items-center w-80 bg-slate-100 rounded-lg p-4 lg:p-8">
               <div className="w-24 md:w-32 h-24 md:h-32 bg-slate-200 rounded-full overflow-hidden shadow-lg mb-2 md:mb-4">
                 <img src={avatar} loading="lazy" alt={fullName} className="w-full h-full object-cover object-center" />
               </div>
