3322dde4767771551f8016a7d24b707605e2778a
Dejan Vasic <dejanvasic24@gmail.com>
2022-11-29 00:29:11 +1100

    Changing feedback block background colour

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Testimonial({ fullName, comment, rating }) {
   return (
-    <div className="flex flex-col justify-between items-center bg-primary rounded-lg gap-4 md:gap-6 px-8 py-6">
+    <div className="flex flex-col justify-between items-center bg-slate-500 rounded-lg gap-4 md:gap-6 px-8 py-6">
       <div className="max-w-md text-white lg:text-lg text-center">“{comment}”</div>
 
       <div className="flex flex-col items-center gap-2 md:gap-3">
