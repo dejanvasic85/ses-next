43e9ca7747a81ec34530f3e718f17c70c02dc88a
Dejan Vasic <dejanvasic24@gmail.com>
2022-12-06 22:08:26 +1100

    Removing legacy services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -44,35 +44,3 @@ export const Services = ({ className, services }) => {
     </div>
   );
 };
-
-export const ServicesV2 = ({ className, services }) => {
-  return (
-    <div className={className}>
-      <Heading level={2}>Our Services</Heading>
-      <Container>
-        <ul className="grid md:grid-cols-2 gap-6 mt-12">
-          {services.map(({ name, description, icon = 'bolt', imageGallery }, idx) => {
-            const [firstImage] = imageGallery || [];
-            return (
-              <li className="group h-80 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4">
-                <img
-                  src={firstImage.src}
-                  loading="lazy"
-                  alt="Photo by Fakurian Design"
-                  className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200 brightness-75"
-                />
-                <div className="bg-gradient-to-t from-black via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
-                <div className="flex flex-col relative">
-                  <span className="text-gray-300">{name}</span>
-                  <span className="flex items-start gap-2 text-white text-lg lg:text-xl font-semibold">
-                    {description}
-                  </span>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </Container>
-    </div>
-  );
-};
