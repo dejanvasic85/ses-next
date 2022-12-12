441aecaa9e8228dc28d5c023b23becb424edf1ef
Dejan Vasic <dejanvasic24@gmail.com>
2022-12-12 21:51:19 +1100

    Adding google tag manager fixes #4

--- packages/ses-next/src/pages/_app.js
+++ packages/ses-next/src/pages/_app.js
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
 import '../../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (!publicRuntimeConfig.googleTagManagerId) {
+      console.log('Google Tag Manager ID not set, not initializing GTM');
+      return;
+    }
+
+    const tagManagerArgs = {
+      gtmId: publicRuntimeConfig.googleTagManagerId,
+    };
+
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
