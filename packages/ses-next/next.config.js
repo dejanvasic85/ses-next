b4e9feb054eb567fc34b350916bac9e1336f0e12
Dejan Vasic <dejanvasic24@gmail.com>
2024-01-04 13:22:59 +1100

    fix: using new next image config

--- packages/ses-next/next.config.js
+++ packages/ses-next/next.config.js
@@ -6,7 +6,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const nextConfig = {
   images: {
-    domains: [`cdn.sanity.io`],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
   },
   publicRuntimeConfig: {
     googleTagManagerId: process.env.GTM_ID,
