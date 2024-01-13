d7f0a24c0e8207fd18360dc48a3f6ee77c0153f8
Dejan Vasic <dejanvasic24@gmail.com>
2024-01-13 17:41:03 +1100

    Replacing leave feedback button with a link

--- packages/ses-next/src/components/About.jsx
+++ packages/ses-next/src/components/About.jsx
@@ -7,7 +7,6 @@ import { Container } from './Container';
 import { Heading } from './Heading';
 import { FeedbackForm } from './FeedbackForm';
 import { Modal } from './Modal';
-import { PopSuccess } from './PopSuccess';
 import { Team } from './Team';
 import { Testimonial } from './Testimonial';
 
@@ -34,12 +33,15 @@ export function About({ team, testimonials, training }) {
           ))}
         </div>
         <div className="mt-4">
-          <PopSuccess show={feedbackSent}>Thank you! We will review your feedback shortly.</PopSuccess>
           {showFeedbackButton && (
-            <div className="mt-4">
-              <button className={classNames('btn btn-primary btn-outline')} onClick={() => setModalOpen(true)}>
-                Leave feedback
-              </button>
+            <div className="mt-8 flex gap-4 justify-center items-center">
+              <a
+                href="https://www.google.com/maps/place/Storm+Electrical+Solutions/@-37.8354339,144.8650809,15z/data=!4m2!3m1!1s0x0:0xd077705b9fe576ea?sa=X&ved=2ahUKEwjz-pjU39mDAxUEcmwGHTuOB8sQ_BJ6BAhFEAA"
+                target="_blank"
+                className={classNames('link link-primary link-hover underline')}
+              >
+                View more reviews on Google
+              </a>
             </div>
           )}
         </div>
