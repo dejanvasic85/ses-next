83ebb4ea05389e4d158ad2db1d0b9ecbc96e2406
Dejan Vasic <dejanvasic24@gmail.com>
2024-05-04 19:24:43 +1000

    blog posts in sitemap

--- packages/ses-next/src/pages/sitemap.xml.js
+++ packages/ses-next/src/pages/sitemap.xml.js
@@ -1,5 +1,5 @@
-import { getHomePageContent } from '../lib/content/contentService';
-import { buildFetchFromApi, fetchFromApi } from '../lib/content/contentApi';
+import { getHomePageContent, getBlogPosts } from '../lib/content/contentService';
+import { buildFetchFromApi } from '../lib/content/contentApi';
 
 const createLocXmlForUrl = (url) => `
     <url>
@@ -7,7 +7,7 @@ const createLocXmlForUrl = (url) => `
     </url>
 `;
 
-const generateSitemap = (content) => {
+const generateSitemap = (content, blogPosts) => {
   const {
     baseUrl,
     services: { items = [] },
@@ -15,11 +15,13 @@ const generateSitemap = (content) => {
 
   const knownPages = ['', 'faq'].map((page) => createLocXmlForUrl(`${baseUrl}${page}`)).join(' ');
   const servicePages = items.map(({ slug }) => createLocXmlForUrl(`${baseUrl}services/${slug}`)).join(' ');
+  const blogPostUrls = blogPosts.map(({ slug }) => createLocXmlForUrl(`${baseUrl}blog/${slug}`)).join(' ');
 
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${knownPages}
     ${servicePages}
+    ${blogPostUrls}
    </urlset>
  `;
 };
@@ -29,8 +31,9 @@ const Sitemap = () => {};
 export const getServerSideProps = async ({ res }) => {
   console.log('Page: sitemap getStaticProps');
   const content = await getHomePageContent(buildFetchFromApi);
+  const blogPosts = await getBlogPosts();
 
-  const sitemap = generateSitemap(content);
+  const sitemap = generateSitemap(content, blogPosts);
 
   res.setHeader('Content-Type', 'text/xml');
   // we send the XML to the browser
