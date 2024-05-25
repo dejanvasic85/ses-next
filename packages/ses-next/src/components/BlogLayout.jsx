0128884d40d0c326d9b41dafae40e510a5a8a6e7
Dejan Vasic <dejanvasic24@gmail.com>
2024-05-25 10:34:08 +1000

    Fixing the blog layout

--- packages/ses-next/src/components/BlogLayout.jsx
+++ packages/ses-next/src/components/BlogLayout.jsx
@@ -3,7 +3,7 @@ import { BlogMenu } from './BlogMenu';
 export const BlogLayout = ({ tags, children }) => {
   return (
     <div className="flex min-h-[50vh] w-full flex-col mt-6 justify-center gap-6 lg:flex-row container mx-auto">
-      <section className="max-w-2xl max-lg:mx-auto max-lg:w-full">
+      <section className="max-w-lg max-lg:mx-auto max-lg:w-full">
         <BlogMenu tags={tags} />
       </section>
       {children}
