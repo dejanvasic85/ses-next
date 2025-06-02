272331a91a256cd5c3866cb1a16aee9265ba4419
Dejan Vasic <dejanvasic24@gmail.com>
2025-06-02 23:07:18 +1000

    Fixing the menu to show the call icon for mobile view

--- packages/ses-next/src/components/Navbar.tsx
+++ packages/ses-next/src/components/Navbar.tsx
@@ -6,8 +6,7 @@ import { Container, Icon } from '.';
 
 const styles = {
   nav: {
-    default:
-      'sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100',
+    default: 'sticky top-0 z-30 w-full bg-opacity-90 backdrop-blur transition-all duration-100',
     top: 'text-base-content',
     scrolled: 'bg-base-100 text-base-content shadow-sm',
   },
@@ -42,57 +41,64 @@ export function Navbar({
 }: NavbarProps) {
   return (
     <nav className={classNames(styles.nav.default, styles.nav.scrolled)}>
-      <nav className="navbar w-full">
-        <Container>
+      <Container>
+        <div className="flex items-center justify-between h-full w-full">
           <div className="hidden md:flex md:flex-1">
-            <a className="btn btn-ghost normal-case text-xl" href={links.home}>
+            <NextLink className="btn btn-ghost normal-case text-xl" href={links.home}>
               <Icon name="bolt" size="xxl" className="mr-2" /> {title}
-            </a>
+            </NextLink>
           </div>
-          <div className="flex items-center justify-center text-sm md:text-base w-full md:w-auto md:flex-none">
-            <ul className="menu menu-xs lg:menu-md menu-horizontal p-0">
-              <li className="">
-                <NextLink className="rounded-lg" href="/">
-                  Home
-                </NextLink>
-              </li>
-              <li>
-                <NextLink className="rounded-lg" href={links.services}>
-                  Services
-                </NextLink>
-              </li>
-              <li>
-                <NextLink className="rounded-lg" href={links.about}>
-                  About Us
-                </NextLink>
-              </li>
-              <li>
-                <NextLink className="rounded-lg" href={links.contact}>
-                  Contact Us
-                </NextLink>
-              </li>
-              <li>
-                <NextLink className="rounded-lg" href={links.faq}>
-                  FAQ
-                </NextLink>
-              </li>
-              <li>
-                <NextLink className="rounded-lg" href={links.blog}>
-                  Blog
-                </NextLink>
-              </li>
-              {contactPhone && (
-                <li className="hidden md:inline-block">
-                  <NextLink className="rounded-lg" href={`tel:${contactPhone}`}>
-                    <Icon name="phone" />
-                    {contactPhone}
-                  </NextLink>
-                </li>
-              )}
-            </ul>
-          </div>
-        </Container>
-      </nav>
+          <Menu>
+            <MenuLinkItem href={links.home} className="flex items-center gap-1">
+              <Icon name="home" className="lg:hidden" />
+              <span className="hidden lg:block">Home</span>
+            </MenuLinkItem>
+            <MenuLinkItem href={links.services}>Services</MenuLinkItem>
+            <MenuLinkItem href={links.about}>About</MenuLinkItem>
+            <MenuLinkItem href={links.contact}>Contact</MenuLinkItem>
+            <MenuLinkItem href={links.faq}>FAQ</MenuLinkItem>
+            <MenuLinkItem href={links.blog}>Blog</MenuLinkItem>
+            {contactPhone && (
+              <MenuLinkItem href={`tel:${contactPhone}`} className="flex items-center gap-1">
+                <Icon name="phone" />
+                <span className="hidden lg:block">{contactPhone}</span>
+              </MenuLinkItem>
+            )}
+          </Menu>
+        </div>
+      </Container>
     </nav>
+  );
+}
+
+interface MenuLinkItemProps extends React.PropsWithChildren {
+  href: string;
+  className?: string;
+}
+
+function MenuLinkItem({ href, children, className }: MenuLinkItemProps) {
+  return (
+    <li>
+      <NextLink href={href} className={classNames('hover:bg-slate-500 hover:text-white rounded p-2', className)}>
+        {children}
+      </NextLink>
+    </li>
+  );
+}
+
+interface MenuProps extends React.PropsWithChildren {
+  className?: string;
+}
+
+function Menu({ children, className }: MenuProps) {
+  return (
+    <ul
+      className={classNames(
+        'flex items-center h-16 text-xs md:text-sm justify-center w-full gap-1 lg:gap-2 md:w-auto md:flex-none',
+        className,
+      )}
+    >
+      {children}
+    </ul>
   );
 }
