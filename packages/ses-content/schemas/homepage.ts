e9845bb1935e75786d527b4374cbbc57bec4887c
Dejan Vasic <dejanvasic24@gmail.com>
2025-12-13 23:26:34 +1100

    removing the old homepage fields

--- packages/ses-content/schemas/homepage.ts
+++ packages/ses-content/schemas/homepage.ts
@@ -6,19 +6,6 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'companyName',
-      type: 'string',
-      title: 'Company name',
-      description: 'Hint: The main title in the homepage',
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
-      name: 'companyLogo',
-      type: 'image',
-      title: 'Company Logo',
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
       name: 'mainHeading',
       type: 'string',
       title: 'Main Heading',
@@ -31,19 +18,11 @@ export default defineType({
       description: 'Hint: Appears below the main heading',
     }),
     defineField({
-      name: 'shortTitle',
-      type: 'string',
-      title: 'Short Title',
-      description: 'Hint: Appears in the desktop navbar',
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
       title: 'About blurbs',
       name: 'about',
       type: 'array',
       of: [{type: 'text', rows: 4}],
     }),
-    defineField({title: 'Base URL', name: 'baseUrl', type: 'url', description: 'Your domain name'}),
     defineField({
       title: 'Contact',
       name: 'contact',
@@ -63,48 +42,6 @@ export default defineType({
           description: 'Appears in the contact form',
         },
         {name: 'phone', type: 'string', title: 'Phone number'},
-      ],
-    }),
-    defineField({
-      title: 'Google Maps location embedded url',
-      description:
-        'You can get this by going to google maps doing a search and clicking share. Then click embed map and copy the url.',
-      name: 'googleMapsLocation',
-      type: 'url',
-    }),
-    defineField({
-      title: 'Google Maps place url',
-      description:
-        'This is the actual url of the google place and you get it from the url of the google maps location. It should look something like this: https://www.google.com/maps/place/Your+Company+Name/@51.',
-      name: 'googleMapsLocationPlaceUrl',
-      type: 'url',
-    }),
-    defineField({
-      title: 'Meta',
-      name: 'meta',
-      type: 'object',
-      description: 'These are important for Google seo and social media',
-      fields: [
-        {
-          name: 'title',
-          type: 'string',
-          title: 'Title',
-        },
-        {
-          name: 'description',
-          type: 'string',
-          title: 'Description',
-        },
-      ],
-    }),
-    defineField({
-      title: 'Social media',
-      name: 'socialMedia',
-      type: 'object',
-      fields: [
-        {name: 'facebook', type: 'url', title: 'Facebook'},
-        {name: 'linkedIn', type: 'url', title: 'LinkedIn'},
-        {name: 'instagram', type: 'url', title: 'Instagram'},
       ],
     }),
     defineField({
